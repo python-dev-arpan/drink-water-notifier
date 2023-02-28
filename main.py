403d976a5c2bc7e3883430a0933304123b46cf6e
Arpan <pythondevarpan@gmail.com>
2023-02-28 17:56:34 +0530

    Music is played for each iteration now

--- main.py
+++ main.py
@@ -1,37 +1,23 @@
 import time
-import threading
+from threading import Thread
 from win10toast import ToastNotifier
 from playsound import playsound
 
 notification = ToastNotifier()
 
 
-def notification_function():
-    while True:
-        notification.show_toast(
-            title="Drink Water Notifier",
-            msg="We should drink about 15.5 cups (3.7 liters) of fluids a day for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
-            icon_path="C:/Users/Karmakar's PC/Documents/Arpan Karmakar/Python Programming/projects/drink_water_notifier/water.ico",
-            duration=10,
-        )
-        time.sleep(1800)
-
-
-def playsound_function():
+def playsound_fx():
     playsound(
         "C:/Users/Karmakar's PC/Documents/Arpan Karmakar/Python Programming/projects/drink_water_notifier/water.wav"
     )
 
 
-# ChatGPT assisted code:
-
-notification_thread = threading.Thread(target=notification_function)
-playsound_thread = threading.Thread(target=playsound_function)
-
-# Start both threads
-notification_thread.start()
-playsound_thread.start()
-
-# Wait for both threads to finish
-notification_thread.join()
-playsound_thread.join()
+while True:
+    Thread(target=playsound_fx).start()
+    notification.show_toast(
+        title="Drink Water Notifier",
+        msg="We should drink about 15.5 cups (3.7 liters) of fluids a day for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
+        icon_path="C:/Users/Karmakar's PC/Documents/Arpan Karmakar/Python Programming/projects/drink_water_notifier/water.ico",
+        duration=10,
+    )
+    time.sleep(1800)
